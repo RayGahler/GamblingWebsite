d1f75fa2d3ce7594f6246f1d87805ad9a378189f
RayGahler <rgahler2003@gmail.com>
2025-06-10 14:25:46 -0700

    http not https

--- src/GetBackend.js
+++ src/GetBackend.js
@@ -1,7 +1,7 @@
 import {io} from "socket.io-client"
 
 
-export const ws = io("gamblingwebsite:5000", {
+export const ws = io("http://gamblingwebsite:5000", {
     cors: {
         origin: "*", //your own :port or a "*" for all origins
     }})
